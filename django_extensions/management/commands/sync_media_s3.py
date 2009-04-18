cb18dbb5fc352b4fd1ac9696a15724466915367b
v.oostveen <v.oostveen@70537768-783f-0410-b1a0-8399f7a62752>
2009-04-18 18:08:20 +0000

    fixed merge conflict in sync_media_s3, please do not use the svn repository at code.google.com anymore, please move to github for our current development version. reverted to the older version of sync_media_s3
    git-svn-id: http://django-command-extensions.googlecode.com/svn/trunk@196 70537768-783f-0410-b1a0-8399f7a62752

--- django_extensions/management/commands/sync_media_s3.py
+++ django_extensions/management/commands/sync_media_s3.py
@@ -25,17 +25,9 @@ Command options are:
   --expires             Enables setting a far future expires header.
   --force               Skip the file mtime check to force upload of all
                         files.
-<<<<<<< HEAD:django_extensions/management/commands/sync_media_s3.py
 
 TODO:
 * Make FILTER_LIST an optional argument
-=======
-  --filter-list         Override default directory and file exclusion
-                        filters. (enter as comma seperated line)
-
-TODO:
- * Use fnmatch (or regex) to allow more complex FILTER_LIST rules.
->>>>>>> master:django_extensions/management/commands/sync_media_s3.py
 
 """
 import datetime
@@ -45,10 +37,7 @@ import optparse
 import os
 import sys
 import time
-<<<<<<< HEAD:django_extensions/management/commands/sync_media_s3.py
 
-=======
->>>>>>> master:django_extensions/management/commands/sync_media_s3.py
 from django.core.management.base import BaseCommand, CommandError
 
 # Make sure boto is available
@@ -65,11 +54,7 @@ class Command(BaseCommand):
     AWS_SECRET_ACCESS_KEY = ''
     AWS_BUCKET_NAME = ''
     DIRECTORY = ''
-<<<<<<< HEAD:django_extensions/management/commands/sync_media_s3.py
     FILTER_LIST = ['.DS_Store',]
-=======
-    FILTER_LIST = ['.DS_Store', '.svn', '.hg', '.git', 'Thumbs.db']
->>>>>>> master:django_extensions/management/commands/sync_media_s3.py
     GZIP_CONTENT_TYPES = (
         'text/css',
         'application/javascript',
@@ -91,14 +76,7 @@ class Command(BaseCommand):
             help="Enables setting a far future expires header."),
         optparse.make_option('--force',
             action='store_true', dest='force', default=False,
-<<<<<<< HEAD:django_extensions/management/commands/sync_media_s3.py
             help="Skip the file mtime check to force upload of all files.")
-=======
-            help="Skip the file mtime check to force upload of all files."),
-        optparse.make_option('--filter-list', dest='filter_list',
-            action='store', default='',
-            help="Override default directory and file exclusion filters. (enter as comma seperated line)"),
->>>>>>> master:django_extensions/management/commands/sync_media_s3.py
     )
 
     help = 'Syncs the complete MEDIA_ROOT structure and files to S3 into the given bucket name.'
@@ -131,11 +109,6 @@ class Command(BaseCommand):
         else:
             if not settings.MEDIA_ROOT:
                 raise CommandError('MEDIA_ROOT must be set in your settings.')
-<<<<<<< HEAD:django_extensions/management/commands/sync_media_s3.py
-=======
-        
-        self.FILTER_LIST = getattr(settings, 'FILTER_LIST', self.FILTER_LIST)
->>>>>>> master:django_extensions/management/commands/sync_media_s3.py
         self.DIRECTORY = settings.MEDIA_ROOT
 
         self.verbosity = int(options.get('verbosity'))
@@ -143,14 +116,6 @@ class Command(BaseCommand):
         self.do_gzip = options.get('gzip')
         self.do_expires = options.get('expires')
         self.do_force = options.get('force')
-<<<<<<< HEAD:django_extensions/management/commands/sync_media_s3.py
-=======
-        filter_list = options.get('filter_list').split(',')
-        if filter_list:
-            # command line option overrides default filter_list and
-            # settings.filter_list
-            self.FILTER_LIST = filter_list
->>>>>>> master:django_extensions/management/commands/sync_media_s3.py
 
         # Now call the syncing method to walk the MEDIA_ROOT directory and
         # upload all files found.
@@ -197,12 +162,6 @@ class Command(BaseCommand):
         if root_dir == dirname:
             return # We're in the root media folder
 
-<<<<<<< HEAD:django_extensions/management/commands/sync_media_s3.py
-=======
-        if os.path.basename(dirname) in self.FILTER_NAMES:
-            return # Skip directories we don't want to sync
-
->>>>>>> master:django_extensions/management/commands/sync_media_s3.py
         # Later we assume the MEDIA_ROOT ends with a trailing slash
         # TODO: Check if we should check os.path.sep for Windows
         if not root_dir.endswith('/'):
